4e2d2d651148ebdfe8ac54f3f53cde132ade274e
lushuang <lushuang3087@163.com>
2018-05-22 13:51:51 +0800

    spring boot webservices integration

---
+++ spring-boot-webservices/src/main/java/com/lushuang/springbootwebservices/SpringBootWebservicesApplication.java
@@ -0,0 +1,12 @@
+package com.lushuang.springbootwebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebservicesApplication.class, args);
+	}
+}
---
+++ spring-boot-webservices/src/test/java/com/lushuang/springbootwebservices/SpringBootWebservicesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lushuang.springbootwebservices;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootWebservicesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
